555481663fe26a77ef148d80ece493e4f0174154
Daniel Micay <danielmicay@gmail.com>
2022-10-19 21:09:47 -0400

    load AoC firmware via daemon

--- aoc/aoc.mk
+++ aoc/aoc.mk
@@ -9,14 +9,8 @@ PRODUCT_PACKAGES += \
     vendor.google.audiometricext@1.0-service-vendor
 endif
 
-# If AoC Daemon is not present on this build, load firmware at boot via rc
-ifeq ($(wildcard vendor/google/whitechapel/aoc/aocd),)
-PRODUCT_COPY_FILES += \
-	device/google/gs-common/aoc/conf/init.aoc.nodaemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.aoc.rc
-else
 PRODUCT_COPY_FILES += \
 	device/google/gs-common/aoc/conf/init.aoc.daemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.aoc.rc
-endif
 
 # AoC debug support
 PRODUCT_PACKAGES_DEBUG += \
