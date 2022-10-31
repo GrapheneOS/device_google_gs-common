271a36cdcc020a16a2f6451ce91ddfeea21e00a8
George Lee <geolee@google.com>
2022-10-31 19:47:55 +0000

    betterbug: Update selinux policy for master
    Update startup_bugreport_requested property to vendor_public for
    betterbug to access.
    
    Bug: 237287659
    Test: Load Betterbug for accessing startup bugreport reason property
    Change-Id: I7102156fe62d5f03f64c367014c2d1ce9bd679b6

--- battery_mitigation/bcl.mk
+++ battery_mitigation/bcl.mk
@@ -2,3 +2,5 @@ PRODUCT_PACKAGES += battery_mitigation
 
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/battery_mitigation/sepolicy
 PRODUCT_SOONG_NAMESPACES += device/google/gs-common/battery_mitigation
+
+# BetterBug is included in /vendor/google/products/pixel_experience_2017_and_newer.mk
