2f4d29bceaf6e23d358d5d594995c152365ce5a7
Jackal Guo <jackalguo@google.com>
2023-05-03 15:56:31 +0800

    Create sepolicy for Pixel System Service
    Having the dedicated domain for Pixel System Service instead of
    using platform_app.
    
    Test: m
    Bug: 274749278
    Change-Id: I852fc46a3b811925e3a75e71a3579b58898045f9

---
+++ pixelsystemservice/pixelsystemservice.mk
@@ -0,0 +1,5 @@
+PRODUCT_SOONG_NAMESPACES += vendor/google/apps/PersistentBackgroundServices
+PRODUCT_PACKAGES += \
+       PersistentBackgroundServices
+
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/pixelsystemservice/sepolicy
