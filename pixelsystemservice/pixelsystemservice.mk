9c832d795e6a33f3246fd5692b66dd0f726e4a70
Jackal Guo <jackalguo@google.com>
2023-08-21 09:51:48 +0800

    Revert "Create sepolicy for Pixel System Service"
    This reverts commit 2f4d29bceaf6e23d358d5d594995c152365ce5a7.
    
    Reason for revert: no need it anymore.
    
    Test: m
    Bug: 296508201
    Change-Id: I51e632bb93343ec6240819207c2bada64b2df2bb

--- pixelsystemservice/pixelsystemservice.mk
+++
@@ -1,5 +0,0 @@
-PRODUCT_SOONG_NAMESPACES += vendor/google/apps/PersistentBackgroundServices
-PRODUCT_PACKAGES += \
-       PersistentBackgroundServices
-
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs-common/pixelsystemservice/sepolicy
