85f9b1e5bc9320a4ab8dc3f6d26027dba76882c6
Inseob Kim <inseob@google.com>
2023-09-05 13:13:01 +0900

    Move camera's coredomain sepolicy to product
    Because they are installed to product partition and it's Treble
    violation to assign them with vendor sepolicy
    
    Bug: 296512192
    Test: lunch panther and build
    Change-Id: I2d2c2a8027eed2b3e2ee1a78d629d44b99867128

--- camera/dump.mk
+++ camera/dump.mk
@@ -1,4 +1,6 @@
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/camera/sepolicy/
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/camera/sepolicy/vendor
+PRODUCT_PUBLIC_SEPOLICY_DIRS += device/google/gs-common/camera/sepolicy/product/public
+PRODUCT_PRIVATE_SEPOLICY_DIRS += device/google/gs-common/camera/sepolicy/product/private
 
 PRODUCT_PACKAGES_DEBUG += dump_camera
 
