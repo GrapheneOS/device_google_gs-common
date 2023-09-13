019653dd200702724c34dcfe3339c73c8c73c587
Inseob Kim <inseob@google.com>
2023-09-13 04:24:29 +0000

    Move brownout_detection definition to system_ext
    Because brownout detection app is installed to system_ext.
    
    Bug: 296512192
    Test: build panther and boot
    Test: Automatically filed b/300277478
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:155ec7653f2aaf01ca0495c2fcd51ea5728bbb04)
    Merged-In: Ia381f01bbfeb8576a4f9b58ca24aeeeb436060b7
    Change-Id: Ia381f01bbfeb8576a4f9b58ca24aeeeb436060b7

--- battery_mitigation/bcl.mk
+++ battery_mitigation/bcl.mk
@@ -3,5 +3,7 @@ ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 PRODUCT_PACKAGES += BrownoutDetection
 endif
 
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/battery_mitigation/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/battery_mitigation/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += device/google/gs-common/battery_mitigation/sepolicy/system_ext/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += device/google/gs-common/battery_mitigation/sepolicy/system_ext/public
 PRODUCT_SOONG_NAMESPACES += device/google/gs-common/battery_mitigation
