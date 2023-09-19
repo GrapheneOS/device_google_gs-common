eec4beedd6daad60df5fe2e2c886469e0dfec1d1
Klines Jiang <klinesjiang@google.com>
2023-09-19 03:50:44 +0000

    [SELinux] Skip build gyotaku_app doamin to AOSP target.
    Bug: 299864020
    
    Test: Local build and tested pass.
    Change-Id: I49772ad19da5d35c418adcb6b19278c02474ecd0

--- gyotaku_app/gyotaku.mk
+++ gyotaku_app/gyotaku.mk
@@ -1,6 +1,8 @@
 ifneq ($(TARGET_BUILD_VARIANT), user)
-  PRODUCT_PACKAGES_DEBUG += \
+  ifeq (,$(filter aosp_%, $(TARGET_PRODUCT))) # Skip aosp target product
+    PRODUCT_PACKAGES_DEBUG += \
       Gyotaku
 
-  BOARD_SEPOLICY_DIRS += device/google/gs-common/gyotaku_app/sepolicy/
+      BOARD_SEPOLICY_DIRS += device/google/gs-common/gyotaku_app/sepolicy/
+  endif
 endif
