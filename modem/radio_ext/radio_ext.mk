cbc4b64bd1bff8211ef43f8d32856bb342dafa52
Alan Chen <alanblc@google.com>
2023-09-19 03:45:02 +0000

    Add radio_ext aidl to compatibility matrix
    Test: manual
    Bug: 285459428
    Change-Id: Ic07926b2af7ea980575f4909ebe742efeb39ef82

---
+++ modem/radio_ext/radio_ext.mk
@@ -0,0 +1,5 @@
+PRODUCT_SOONG_NAMESPACES += vendor/google/interfaces
+PRODUCT_PACKAGES += vendor.google.radio_ext-service
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/modem/radio_ext/compatibility_matrix.xml
+
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/modem/radio_ext/sepolicy
