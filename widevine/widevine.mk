5cf384c56feb857fdf7745cae4ee7b967355169f
Kyle Zhang <kelzhan@google.com>
2023-11-15 00:20:28 +0000

    Check dev-keystore to sign widevine apex
    Bug: 310715038
    Change-Id: I16dc21365d8ceb7ad1dd09afe88be91ef7db34ed

--- widevine/widevine.mk
+++ widevine/widevine.mk
@@ -3,6 +3,9 @@ PRODUCT_PACKAGES += \
 
 ifdef RELEASE_PACKAGE_WIDEVINE
   PRODUCT_PACKAGES += $(RELEASE_PACKAGE_WIDEVINE)
+  ifneq ($(wildcard vendor/google/dev-keystore),)
+    $(call soong_config_set,widevine,use_devkey,true)
+  endif
   PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
 	vendor/widevine/libwvdrmengine/apex/device/linker.config.json
 else
