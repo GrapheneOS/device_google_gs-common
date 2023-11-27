989583723944ec4af3a49a0a080f9086b14bb836
Jason Chiu <jasoncschiu@google.com>
2023-11-27 16:58:15 +0800

    gs-common: move sepolicy related to bootctrl hal aidl to gs-common
    Bug: 265063384
    Change-Id: I8f090f4601e0719ae3abe3e9d0981710fdc8b6af
    Signed-off-by: Jason Chiu <jasoncschiu@google.com>

--- bootctrl/bootctrl_aidl.mk
+++ bootctrl/bootctrl_aidl.mk
@@ -1,3 +1,5 @@
 PRODUCT_PACKAGES += \
 	android.hardware.boot-service.default-pixel \
 	android.hardware.boot-service.default_recovery-pixel
+
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/bootctrl/sepolicy/aidl
