f4d8fb7ad87375f225eae30bfa5f63fba042e03c
Jason Chiu <jasoncschiu@google.com>
2023-11-27 16:50:17 +0800

    gs-common: move sepolicy related to bootctrl hal hidl 1.2 to gs-common
    Bug: 265063384
    Change-Id: I1981edd60a77e4c5aede8649c7b6bc5eacb676ee
    Signed-off-by: Jason Chiu <jasoncschiu@google.com>

--- bootctrl/bootctrl_hidl_1.2.mk
+++ bootctrl/bootctrl_hidl_1.2.mk
@@ -3,3 +3,4 @@ PRODUCT_PACKAGES += \
 	android.hardware.boot@1.2-service-pixel
 
 PRODUCT_SOONG_NAMESPACES += device/google/gs-common/bootctrl/1.2
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/bootctrl/sepolicy/1.2
