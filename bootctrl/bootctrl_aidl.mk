c3102b06fcb95462334e24a9a9a3179ce978c179
Jason Chiu <jasoncschiu@google.com>
2023-11-27 16:53:59 +0800

    gs-common: create bootctrl_aidl.mk and include related modules
    Bug: 265063384
    Change-Id: I9362eb521616c899a14605081fdc10de2a1d5c16
    Signed-off-by: Jason Chiu <jasoncschiu@google.com>

---
+++ bootctrl/bootctrl_aidl.mk
@@ -0,0 +1,5 @@
+PRODUCT_PACKAGES += \
+	android.hardware.boot-service.default-pixel \
+	android.hardware.boot-service.default_recovery-pixel
+
+PRODUCT_SOONG_NAMESPACES += device/google/gs-common/bootctrl/aidl
