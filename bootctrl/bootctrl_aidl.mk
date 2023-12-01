1638fa1a3604a1b2dfc7fa6d857fd74b462efe15
Sebastian Pickl <spickl@google.com>
2023-12-01 11:30:35 +0000

    Revert "gs-common: move sepolicy related to bootctrl hal aidl to..."
    Revert submission 25477883-gs-common_bootctrl-aidl
    
    Reason for revert: breaking builds b/314240126
    
    Bug: 314240126
    
    Reverted changes: /q/submissionid:25477883-gs-common_bootctrl-aidl
    
    Change-Id: I091dda3aa16bbc381b49674a642b2bfa93dfdfa8

--- bootctrl/bootctrl_aidl.mk
+++ bootctrl/bootctrl_aidl.mk
@@ -1,5 +1,3 @@
 PRODUCT_PACKAGES += \
 	android.hardware.boot-service.default-pixel \
 	android.hardware.boot-service.default_recovery-pixel
-
-BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/bootctrl/sepolicy/aidl
